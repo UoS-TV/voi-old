907f7eb2199a1f6317fff89a07d9e0febfb14380
samsarj <samsarj@gmail.com>
2023-09-28 13:32:40 +0100

    improvements

--- csv-processing.py
+++ csv-processing.py
@@ -4,8 +4,10 @@ df = pd.read_csv('User Selection List.csv').query('Active == "Yes"')
 
 df2 = df[(df["Year 1"]!="No")|(df["Year 2"]!="No")|(df["Year 4"]!="No")].replace(to_replace=['Yes', 'No'], value=['\checkmark', ''])
 
-stock = df2.query('`Asset Type` == 0')[['Category','Asset Name','Year 1','Year 2','Year 4']].sort_values(by=['Year 1','Year 2','Year 4','Asset Name'],ascending=[False,False,False,True])
+stock = df2.query('`Asset Type` == 0')[['Category','Asset Name','Asset Description','Year 1','Year 2','Year 4']].sort_values(by=['Year 1','Year 2','Year 4','Asset Name'],ascending=[False,False,False,True])
 rooms = df2.query('`Asset Type` == 1')[['Category','Asset Name','Year 1','Year 2','Year 4']].sort_values(by=['Category','Year 1','Year 2','Year 4','Asset Name'],ascending=[True,False,False,False,True])
+
+print(stock)
 
 for col in stock.columns:
     stock[col] = stock[col].str.replace('&','{\&}')
@@ -26,15 +28,15 @@ dfs = {name: group for name, group in groups}
 for df in dfs.values():
     df.drop(['Category'],inplace=True,axis=1)
 
-print(dfs)
+# print(dfs)
 
-with open('stock.tex', 'w') as file:
+with open('stock.tex', 'w', encoding='utf-8') as file:
     file.write('')
 
-with open('stock.tex', 'a') as file:
+with open('stock.tex', 'a', encoding='utf-8') as file:
     for i in dfs:
         print(i)
-        latex = dfs[i].to_latex(index=False,longtable=True,column_format="p{0.6\\textwidth}ccc",caption=i)
+        latex = dfs[i].to_latex(index=False,longtable=True,column_format="p{0.25\\textwidth}p{0.4\\textwidth}ccc",caption=i,na_rep="")
         print(latex)
         file.write(latex)
      
